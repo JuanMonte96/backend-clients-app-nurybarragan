c5db8919a8dacfaeb3cf5ca6f4a8fab821eccd31
JuanMmonte <juanmaniac96@gmail.com>
2025-10-09 22:15:19 -0500

    block user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,6 +2,7 @@ import { db } from "../models/db.js";
 import { generateToken } from "../services/Jwt.js";
 import { createTempPassword } from "../services/password.js";
 import bcrypt from 'bcrypt';
+import { Op } from "sequelize";
 
 export const createUser = async (session) => {
 
@@ -28,6 +29,7 @@ export const createUser = async (session) => {
             message: 'user created'
         })
     }
+
     return {
         id: user.id_user,
         nombre: user.name_user,
@@ -53,14 +55,62 @@ export const loginUser = async (req, res) => {
                 status: 'Unauthorized',
                 message: 'Invalid password'
             });
+        };
+
+        const t = await db.sequelize.transaction();
+
+        const now = new Date();
+
+        await db.Subscription.update(
+            { status: 'expired' },
+            {
+                where: {
+                    id_user: user.id_user,
+                    status: 'active',
+                    end_date: { [Op.lte]: now },
+                },
+                transaction: t
+            }
+        );
+
+        const activeSubscription = await db.Subscription.count(
+            {
+                where: {
+                    id_user: user.id_user,
+                    status: 'active',
+                    end_date: { [Op.gt]: now },
+                },
+                transaction: t,
+            }
+        );
+
+        if (!activeSubscription) {
+            if (!user.is_blocked) {
+                await db.User.update(
+                    { is_blocked: true },
+                    { where: { id_user: user.id_user }, transaction: t }
+                );
+            }
+            await t.commit();
+            return res.status(403).json({
+                status: 'Forbiden',
+                message: 'Your subscription has expired, please contact to admin or buy a new subscription '
+            })
+        } else if (user.is_blocked) {
+            await db.User.update(
+                { is_blocked: false },
+                { where: { id_user: user.id_user }, transaction: t }
+            );
         }
+
+        await t.commit();
 
         const payload = {
             id: user.id_user,
             email: user.email_user,
             role: user.role,
             must_change_pass: user.must_change_pass
-        }
+        };
 
         const token = generateToken(payload);
 
@@ -68,7 +118,7 @@ export const loginUser = async (req, res) => {
             status: 'success',
             message: 'Login Successful',
             token
-        })
+        });
 
 
     } catch (error) {
@@ -172,7 +222,7 @@ export const changePassword = async (req, res) => {
 
         const user = await db.User.findByPk(userId);
 
-        if(!user){
+        if (!user) {
             return res.status(404).json({
                 status: 'Not Found',
                 message: 'user does not exist'
@@ -180,7 +230,7 @@ export const changePassword = async (req, res) => {
         }
 
         const valid = await bcrypt.compare(currentPassword, user.password_user);
-        if(!valid){
+        if (!valid) {
             return res.status(401).json({
                 status: 'unauthorized',
                 message: 'current password is incorrect'
@@ -188,9 +238,9 @@ export const changePassword = async (req, res) => {
         };
 
         const hashedPassword = await bcrypt.hash(newPassword, 10);
-        
+
         user.password_user = hashedPassword;
-        user.must_change_pass = false; 
+        user.must_change_pass = false;
 
         await user.save();
 
@@ -209,9 +259,9 @@ export const changePassword = async (req, res) => {
 
 export const editUser = async (req, res) => {
     try {
-        const {id_user} = req.params;
+        const { id_user } = req.params;
 
-        const {name_user, email_user, password_user} = req.body;
+        const { name_user, email_user, password_user } = req.body;
 
         const user = await db.User.findByPk(id_user);
 
@@ -230,7 +280,7 @@ export const editUser = async (req, res) => {
 
         if (name_user) user.name_user = name_user;
         if (email_user) user.email_user = email_user;
-        if (password_user){
+        if (password_user) {
             const hashedPassword = await bcrypt.hash(password_user, 10);
             user.password_user = hashedPassword;
             user.must_change_pass = false;
@@ -248,5 +298,50 @@ export const editUser = async (req, res) => {
             status: 'error',
             message: `Internal Server Error: ${error.message}`
         });
+    }
+};
+
+export const blockUser = async (req, res) => {
+    try {
+        const { id_user } = req.params;
+
+        if (req.user.id !== id_user || req.user.role !== 'admin') {
+            return res.status(403).json({
+                status: 'Forbidden',
+                message: 'You do not have permission to block this user'
+            });
+        }
+
+        const user = await db.User.findByPk(id_user);
+        const subscriptionByUser = await db.Subscription.findAll({
+            where: { id_user: id_user, status: 'active' }
+        })
+
+        if (!user) return res.status(404).json({
+            status: 'Not found',
+            message: 'User not found'
+        });
+
+        user.is_blocked = true;
+        // Cancel all active subscriptions for this user
+        for (const sub of subscriptionByUser) {
+            sub.status = 'cancelled';
+            await sub.save();
+        }
+
+        await user.save();
+
+        return res.status(200).json({
+            status: 'success',
+            message: 'User blocked successfully',
+            name: user.name_user
+        });
+
+
+    } catch (error) {
+        return res.status(500).json({
+            status: 'error',
+            message: `Internal Server Error: ${error.message}`
+        })
     }
 };
